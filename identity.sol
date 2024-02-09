4548c1afc154934cc811fadff4b475795b6e7b9c
girdharigodara1105 <girdharigodara1105@gmail.com>
2024-02-09 16:48:32 +0530

    first commit

---
+++ identity.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Identity{
+    string name;
+    uint age;
+    uint public num = 5;
+
+    constructor()  {
+        name = "Girdhari";
+        age = 21;
+    }
+
+    function getName() view public returns (string memory){
+        return name;
+    }
+
+    function getAge () view public returns (uint){
+        return age;
+    }
+
+    function setAge() public{
+        age = age+1;
+    }
+}
